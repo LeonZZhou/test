f55a79d2a280adf3fccf09d2929827861c128878
LeonZZhou <156786363@qq.com>
2017-12-01 22:39:45 +0800

    test

--- flask_web.py
+++ flask_web.py
@@ -12,20 +12,17 @@ c = db.cursor()
 signined = False
 @app.route("/", methods=["GET", "POST"])
 def hello():
-    if signined == True:
-        sql = ""
-        if request.method == "POST":
-            data = request.json
-            try:
-                sql = "INSERT INTO `stat` (`host`,`mem_free`,`mem_usage`,`mem_total`,`load_avg`,`time`) VALUES('%s', '%d', '%d', '%d', '%s', '%d')" % (data['Host'], data['MemFree'], data['MemUsage'], data['MemTotal'], data['LoadAvg'], int(data['Time']))
-                ret = c.execute(sql)
-            except mysql.IntegrityError:
-                pass
-            return "OK"
-        else:
-            return render_template("mon.html")
+    sql = ""
+    if request.method == "POST":
+        data = request.json
+        try:
+            sql = "INSERT INTO `stat` (`host`,`mem_free`,`mem_usage`,`mem_total`,`load_avg`,`time`) VALUES('%s', '%d', '%d', '%d', '%s', '%d')" % (data['Host'], data['MemFree'], data['MemUsage'], data['MemTotal'], data['LoadAvg'], int(data['Time']))
+            ret = c.execute(sql)
+        except mysql.IntegrityError:
+            pass
+        return "OK"
     else:
-        return render_template("signin_form.html")
+        return render_template("mon.html")
 
 @app.route("/data", methods=["GET"])
 def getdata():
