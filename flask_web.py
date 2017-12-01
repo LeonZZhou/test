76ede2b727a66e9bd1cc866fd5e79638c3bddd5f
LeonZZhou <156786363@qq.com>
2017-12-01 18:54:01 +0800

    test

--- flask_web.py
+++ flask_web.py
@@ -9,24 +9,32 @@ db = mysql.connect(user="root", passwd="kop/'.'",\
         db="falcon", charset="utf8")
 db.autocommit(True)
 c = db.cursor()
+signined = False
 @app.route("/", methods=["GET", "POST"])
 def hello():
-    sql = ""
-    if request.method == "POST":
-        data = request.json
-        try:
-            sql = "INSERT INTO `stat` (`host`,`mem_free`,`mem_usage`,`mem_total`,`load_avg`,`time`) VALUES('%s', '%d', '%d', '%d', '%s', '%d')" % (data['Host'], data['MemFree'], data['MemUsage'], data['MemTotal'], data['LoadAvg'], int(data['Time']))
-            ret = c.execute(sql)
-        except mysql.IntegrityError:
-            pass
-        return "OK"
+    if signined == True:
+        sql = ""
+        if request.method == "POST":
+            data = request.json
+            try:
+                sql = "INSERT INTO `stat` (`host`,`mem_free`,`mem_usage`,`mem_total`,`load_avg`,`time`) VALUES('%s', '%d', '%d', '%d', '%s', '%d')" % (data['Host'], data['MemFree'], data['MemUsage'], data['MemTotal'], data['LoadAvg'], int(data['Time']))
+                ret = c.execute(sql)
+            except mysql.IntegrityError:
+                pass
+            return "OK"
+        else:
+            return render_template("mon.html")
     else:
-        return render_template("mon.html")
+        return render_template("signin_form.html")
+
 @app.route("/data", methods=["GET"])
 def getdata():
-    c.execute("SELECT `time`,`mem_usage` FROM `stat`")
-    ones = [[i[0]*1000, i[1]] for i in c.fetchall()]
-    return "%s(%s);" % (request.args.get('callback'), json.dumps(ones))
+    if signined == True:
+        c.execute("SELECT `time`,`mem_usage` FROM `stat`")
+        ones = [[i[0]*1000, i[1]] for i in c.fetchall()]
+        return "%s(%s);" % (request.args.get('callback'), json.dumps(ones))
+    else:
+        return render_template("signin_form.html")
 
 @app.route("/signin",methods=['GET'])
 def signin_form():
@@ -37,7 +45,9 @@ def signin():
     username = request.form['username']
     passwd = request.form['passwd']
     if username == 'admin' and passwd == 'qwer1234':
+        signined = True
         return render_template('mon.html')
+    signined = False
     return render_template('signin_form.html',message='Error user name or passwd!!',uname=username)
 
 @app.route("/<name>",methods=["GET"])
